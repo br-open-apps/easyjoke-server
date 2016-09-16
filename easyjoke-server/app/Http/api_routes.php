a8ec33bd8e6fa307a297b382c95ef4bf87ee2536
Phelipe Alves de Souza <phelipealvesdesouza@gmail.com>
2016-09-15 22:34:31 -0300

    Adicionadno login nas rotas de post, put e delete

--- easyjoke-server/app/Http/api_routes.php
+++ easyjoke-server/app/Http/api_routes.php
@@ -11,24 +11,31 @@ $api->version('v1', function ($api) {
     $api->group(['namespace' => 'App\Api\V1\Controllers', 'middleware' => '\Barryvdh\Cors\HandleCors::class'], function ($api) {
         /** @var \Dingo\Api\Routing\Router $api */
 
-        $api->post('auth/signup', 'AuthController@signup');
+//        $api->post('auth/signup', 'AuthController@signup');
         $api->post('auth/login', 'AuthController@login');
-        $api->post('auth/recovery', 'AuthController@recovery');
-        $api->post('auth/reset', 'AuthController@reset');
+//        $api->post('auth/recovery', 'AuthController@recovery');
+//        $api->post('auth/reset', 'AuthController@reset');
 
         // categories
         $api->get('categories', 'CategoryController@index');
-        $api->post('categories', 'CategoryController@store');
         $api->get('categories/{categories}', 'CategoryController@show');
         $api->get('categories/{categories}/jokes', 'CategoryController@jokes');
-        $api->put('categories/{categories}', 'CategoryController@update');
-        $api->delete('categories/{categories}', 'CategoryController@destroy');
 
         // jokes
         $api->get('jokes', 'JokeController@index');
-        $api->post('jokes', 'JokeController@store');
         $api->get('jokes/{jokes}', 'JokeController@show');
-        $api->put('jokes/{jokes}', 'JokeController@update');
-        $api->delete('jokes/{jokes}', 'JokeController@destroy');
+
+        $api->group(['middleware' => ['api.auth']], function ($api) {
+            // categories
+            $api->post('categories', 'CategoryController@store');
+            $api->put('categories/{categories}', 'CategoryController@update');
+            $api->delete('categories/{categories}', 'CategoryController@destroy');
+
+            // jokes
+            $api->post('jokes', 'JokeController@store');
+            $api->put('jokes/{jokes}', 'JokeController@update');
+            $api->delete('jokes/{jokes}', 'JokeController@destroy');
+        });
+
     });
 });
